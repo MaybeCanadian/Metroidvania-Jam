322041bbf9b52969ca9d00952f851f279d7433d5
MaybeCanadian <1coffeye@gmail.com>
2023-06-02 18:12:41 -0400

    added some more framework for input checking, looking to do directional. Planning to do some movement next. Added component checking for anims and rb.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Event Dispatchers
+
+    #region Generic
+    public delegate void PlayerEventGeneric();
+    public static PlayerEventGeneric OnPlayerSpawned;
+    public static PlayerEventGeneric OnPlayerDeath;
+    #endregion
+
+    #endregion
+
+    //------------------------------
+
+    #region Member Variables
+
+    #region Components
+    [Header("Components")]
+    [SerializeField, Tooltip("The connected Rigid Body")]
+    private Rigidbody2D rb;
+    [SerializeField, Tooltip("Should the controller try and find a connected RB in the object.")]
+    private bool findRigidBodyIfNull = true;
+    [SerializeField, Tooltip("Should the controller check inside the children for the Rigid Body.")]
+    private bool checkChildForRigidBody = true;
+
+    [SerializeField, Tooltip("The connected Animator")]
+    private Animator anims;
+    [SerializeField, Tooltip("Should the controller try and find a connected Animator in the object.")]
+    private bool findAnimsIfNull = true;
+    [SerializeField, Tooltip("Should the controller check inside the children for the Animator")]
+    private bool checkChildForAnims = true;
+    #endregion
+
+    #region Inputs
     [Header("Inputs")]
     [SerializeField, Tooltip("The update cycle that inputs will be polled in.")]
     private UpdateTypes inputPollingUpdateType = UpdateTypes.Update;
@@ -12,31 +44,101 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField, Tooltip("The current recorded input for movement, this is not normalized.")]
     private Vector2 currentMovementInput = Vector2.zero;
+    [SerializeField, Tooltip("The current recorded input for movement, this is not normalized")]
+    private Vector2 currentDirectionalInput = Vector2.zero;
+
     [SerializeField, Tooltip("Should the movement inputs be checked")]
     private bool pollMovementInput = true;
     [SerializeField, Tooltip("Should the mouse inputs me checked")]
     private bool pollDirectionalInput = true;
 
+
+    #endregion
+
+    #region Movements
     [Header("Movement")]
     [SerializeField, Tooltip("The update cycle that movement will occur in.")]
     private UpdateTypes movementUpdateType = UpdateTypes.FixedUpdate;
 
     [SerializeField, Tooltip("How fast the player will move when input is pressed.")]
     private float moveSpeed = 5.0f;
+    #endregion
 
+    #region Animations
     [Header("Animations")]
     [SerializeField, Tooltip("The update cycle the animations will be updated in.")]
     private UpdateTypes animationUpdateType = UpdateTypes.LateUpdate;
 
     [SerializeField, Tooltip("The name of the parameter the animator is checking for.")]
     private string animationParamter = "AnimState";
+    #endregion
+
+    #endregion
+
+    //------------------------------
 
     #region Init Functions
     private void Start()
     {
         currentMovementInput = Vector2.zero;
+
+        ConnectComponents();
+    }
+    private void ConnectComponents()
+    {
+        ConnectRB();
+
+        ConnectAnims();
+    }
+    private void ConnectRB()
+    {
+        if(rb != null)
+        {
+            return;
+        }
+
+        if(findRigidBodyIfNull == false)
+        {
+            Debug.LogError("ERROR - RB for the player controller is null.");
+            return;
+        }
+
+        rb = (checkChildForRigidBody == true) ? GetComponentInChildren<Rigidbody2D>() : GetComponent<Rigidbody2D>();
+
+        if(rb != null)
+        {
+            return;
+        }
+
+        Debug.LogError("ERROR - Could not locate a Rigid Body in the player.");
+        return;
+    }
+    private void ConnectAnims()
+    {
+        if(anims != null)
+        {
+            return;
+        }
+
+        if(findAnimsIfNull == false)
+        {
+            Debug.LogError("ERROR - Anims for player is null.");
+            return;
+        }
+
+        anims = (checkChildForAnims == true) ? GetComponentInChildren<Animator>() : GetComponent<Animator>();
+
+        if(anims != null)
+        {
+            return;
+        }
+
+        Debug.LogError("ERRIR - Could not locate an animator in the player.");
+        return;
     }
     #endregion
+
+    //------------------------------
 
     #region Updates
     private void Update()
@@ -98,6 +200,8 @@ public class PlayerController : MonoBehaviour
     }
     #endregion
 
+    //------------------------------
+
     #region Input
     private void InputPollingUpate(float delta)
     {
@@ -111,6 +215,8 @@ public class PlayerController : MonoBehaviour
             PollDirectionalInput(delta);
         }
     }
+
+    #region Movement Inputs
     private void PollMovementInput(float delta)
     {
         if (currentInputType == InputTypes.KeyboardAndMouse)
@@ -135,20 +241,51 @@ public class PlayerController : MonoBehaviour
     }
     private void PollControllerMovementInput(float delta)
     {
-        //controller stuff goes here
+        //poll left stick input for this control type
     }
+    #endregion
+
+    #region Directional Inputs
     private void PollDirectionalInput(float delta)
+    {
+        if(currentInputType == InputTypes.KeyboardAndMouse)
+        {
+            PollMouseDirectionalInput(delta);
+            return;
+        }
+
+        if(currentInputType == InputTypes.Controller)
+        {
+            PollControllerDirectionalInput(delta);
+            return;
+        }
+    }
+    private void PollMouseDirectionalInput(float delta)
     {
 
     }
+    private void PollControllerDirectionalInput(float delta)
+    {
+        //poll right stick for this control type
+    }
     #endregion
+
+    #endregion
+
+    //------------------------------
 
     #region Movement
     private void MovementUpdate(float delta)
     {
-
+        PlayerBasicMove(delta);
+    }
+    private void PlayerBasicMove(float delta)
+    {
+        
     }
     #endregion
+
+    //------------------------------
 
     #region Animations
     private void AnimationUpdate(float delta)
@@ -156,6 +293,8 @@ public class PlayerController : MonoBehaviour
 
     }
     #endregion
+
+    //------------------------------
 }
 
 #region Enums
